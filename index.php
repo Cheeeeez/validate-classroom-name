26ffc9a9daf54f92e96c77b9019496664ade97ca
Nam <nguyentrongnama8@gmail.com>
2020-04-24 17:45:42 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+function checkClassroomName($str)
+{
+    $regex = '/(^C|A|P)[\d]{4}(G|H|I|K|L|M)$/';
+    if (preg_match($regex, $str)) {
+        echo "Classroom's name is valid";
+    } else {
+        echo "Classroom's name is invalid";
+    }
+}
+
+checkClassroomName('C0318G');
+echo "<br>";
+checkClassroomName('M0318G');
+echo "<br>";
+checkClassroomName('P0323A');
+echo "<br>";
